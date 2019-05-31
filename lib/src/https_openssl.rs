116f4f2ce8c4adc6574c8c13de4878b79a527acd
Geoffroy Couprie <geo.couprie@gmail.com>
2019-05-31 18:07:22 +0200

    add a variant of the trie that supports regexp matches

--- lib/src/https_openssl.rs
+++ lib/src/https_openssl.rs
@@ -1807,7 +1807,7 @@ mod tests {
   use std::rc::Rc;
   use std::sync::{Arc,Mutex};
   use protocol::http::answers::DefaultAnswers;
-  use trie::TrieNode;
+  use router::trie::TrieNode;
   use openssl::ssl::{SslContext, SslMethod};
 
   /*
